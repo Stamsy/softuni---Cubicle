2cfbbcf9046fb3772d8d4c3b8660794b2e5dc3c2
Stanimira Rikova <stams_89@abv.bg>
2023-02-11 10:38:00 +0200

    add cubecontroller

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,11 @@
 
 exports.getCreateCube = (req,res) => {
     res.render('create');
-}
+};
+ 
+exports.postCreateCube = (req, res) => {
+    let cube = new Cube(req.body);
+
+    // redirect
+    res.redirect('/');
+};
