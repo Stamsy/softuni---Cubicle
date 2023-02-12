54873e5044a71cb345df874329c440ea62f8ee68
Stanimira Rikova <stams_89@abv.bg>
2023-02-12 12:38:37 +0200

    save cube to db

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
     production: {
         PORT: 1234,
-        DB_URI: 'mongodb://127.0.0.1:27017/cubicle'
+        DB_URI: 'mongodb://localhost:27017/cubicle'
     },
     development: {
         PORT: 5000,
