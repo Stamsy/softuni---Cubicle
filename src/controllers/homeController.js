a1f50e59be7c33eec588ca9fb0e620edd2cf8924
Stanimira Rikova <stams_89@abv.bg>
2023-02-12 11:00:40 +0200

    add search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,28 @@
 const db = require('../db.json');
 
 exports.getHomePage = (req, res) => {
-    res.render('index', { cubes: db.cubes });// to the index file we pass all the cubes recorded in db.json
+    const { search, from: difficultyFrom, to: difficultyTo } = req.query;
+    let cubes = db.cubes;
+
+    if(search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if(difficultyFrom) {
+        cubes = cubes.filter(cube => cube.difficultyLevel >= difficultyFrom);
+    }
+
+    if(difficultyTo) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= difficultyTo);
+    }
+
+    res.render('index', { cubes, search, difficultyFrom, difficultyTo});// to the index file we pass all the cubes recorded in db.json
 };
 
 exports.getAboutPage = (req, res) => {
     res.render('about');
 };
+
+exports.getErrorPage = (req, res) => {
+    res.render('404');
+}
