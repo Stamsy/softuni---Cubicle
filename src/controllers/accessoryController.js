5895b62ffe7a757357c3e158db0ffe659ae3d0ac
Stanimira Rikova <stams_89@abv.bg>
2023-02-12 13:43:19 +0200

    add accessory to db

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 
-
+const Accessory = require('../models/Accessory');
 //URL: /accessory/create
 router.get('/create', (req,res) => {
     res.render('accessory/create')
 
 });
 
+router.post('/create', async (req,res)=> {
+    const { name, description, imageUrl} = req.body;
+
+    await Accessory.create({ name, description, imageUrl});
+
+    res.redirect('/');
+
+});
 
 module.exports = router;
